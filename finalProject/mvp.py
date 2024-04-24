1f96e788050e23d63a5258ae55e014d2d71b5366
Zayn Patel <zaynpatelwhs@gmail.com>
2024-04-24 01:42:18 -0400

    Refactored the initial context manager function, deleted the 6 handle_heading() functions I had, code is smaller now

--- finalProject/mvp.py
+++ finalProject/mvp.py
@@ -3,176 +3,142 @@ import re
 # Learning 1: Inner loop to define parsing logic, break once first match, keep outer loop to iterate through entire file.
 # Learning 6: Python evaluates and before or so this expression. 
 
+#TODO: Figure out how adding the class changes the input of line -> this will be connected to the read_mardown function
+class HTMLParser:
+    def __init__(self):
+        self.list_of_dictionaries = []
+        self.markdown_handlers = {
+        # Online suggested using the lambda functions because we can pass the line and level now, evaluate simpler than before
+        '# ' : lambda line: self.handle_heading(line, 1), 
+        '## ' : lambda line: self.handle_heading(line, 2), 
+        '### ' : lambda line : self.handle_heading(line, 3), 
+        '#### ' : lambda line: self.handle_heading(line, 4), 
+        '##### ' : lambda line: self.handle_heading(line, 5),
+        '###### ' : lambda line: self.handle_heading(line, 6),
+        '``' : self.handle_inline_block,  
+        '```' : self.handle_code_block, # this is state-based (hard)
+        '- ' : self.handle_ullist_block, # this is state-based (hard and review this because there are multiple symbols we can use to define an ul list, also look at ol list syntax)
+        '![' : self.handle_image, 
+        '> ' : self.handle_blockquote,
+        '**' : self.handle_bold, # this is state-based
+        '*' : self.handle_italic,
+        '[' : self.handle_links
+    }
 
-# Context manager to read lines
-def read_markdown(file):
-    with open(file, 'r') as md:
-        markdownFile = md.readlines()
-        for text in markdownFile:
-            for pattern, function in markdown_handlers.items():
-                if text.startswith(pattern):
-                    function(text)
-
-# need to think more about what to add here and how this relates to parsing and markdown_handlers dictionary (what fcn calling across file looks like too.) 
-list_of_dictionaries = [] # this becomes the immediate representation which I'll pass into my HTML function
-
-# Input to this is one line - where is line in file? Need to write this so it's available and can be parsed. 
-def handle_heading1(line):
-    list_of_dictionaries.append({
-        'type' : 'heading1',
-        'content' : line.lstrip('# ').strip()
-    })
-
-def handle_heading2(line):
-    list_of_dictionaries.append({
-        'type' : 'heading2',
-        'content' : line.lstrip('## ').strip() # why do we need two .strip() - one for heading/one for whitespace after text?
-    })
-
-def handle_heading3(line):
-    list_of_dictionaries.append({
-        'type' : 'heading3',
-        'content' : line.lstrip('### ').strip() 
-    })
-
-def handle_heading4(line):
-    list_of_dictionaries.append({
-        'type' : 'heading4',
-        'content' : line.lstrip('#### ').strip() 
-    })
-
-def handle_heading5(line):
-    list_of_dictionaries.append({
-        'type' : 'heading5',
-        'content' : line.lstrip('##### ').strip() 
-    })
-
-def handle_heading6(line):
-    list_of_dictionaries.append({
-        'type' : 'heading6',
-        'content' : line.lstrip('###### ').strip() 
-    })
-
-def handle_blockquote(line):
-    list_of_dictionaries.append({
-        'type' : 'blockquote',
-        'content' : line.lstrip('> ').strip()
-    })
-
-def handle_links(line):
-    # We need to introduce control flow because there's a limited amount we can do with dictionaries and custom strings
-    # So, anything that matches '[' will be "flagged" as a link. We want to check if this is actually a link or not so we evaluate using if.
-    # if http or https is in the line, evaluate it like a link 
-    # recall that commands for re.search(expression, applied to what)
-    if re.search(r'(http[s]?:\/\/[^\)]+)\)', line): # r here means raw strings
-        pattern = re.findall(r'\[([^\]]+)\]\((http[s]?:\/\/[^\)]+)\)', line) # I originally used .search() but changed to .findall() in the case there are multiple links in a given line.  
-        for link_text, url in pattern: # iterate as a method to unpack tuple created in pattern
-            list_of_dictionaries.append({
-                'type' : 'link', 
-                'content' : {'link text' : link_text, 'url' : url}
-            })
-
-def handle_image(line):
-    # I want to search the string to see if I can find any image file types
-    # Learning 5: Turns out we cannot add lists to regex, have to use the or operator instead.
-    if re.search(r'(https?:\/\/[^\s]+?(?:\.png|\.jpg|\.jpeg|\.gif|\.bmp|\.tiff|\.svg))\)', line):
-        match = re.findall(r'!\[([^\]]*)\]\((https?:\/\/[^\s]+?(?:\.png|\.jpg|\.jpeg|\.gif|\.bmp|\.tiff|\.svg))\)', line)
-        for alt_text, image_url in match:
-            list_of_dictionaries.append({
-                'type' : 'image',
-                'content' : {'alt text' : alt_text, 'image url' : image_url}
-            })        
-
-# Needed help/review for this function. State managing was tricky. 
-def handle_italic(line):
-    in_italic = False # set the state to False because we haven't encountered asterik yet
-    holding_string = "" # this is the string that gets appended to dictionary
-    for text in line:
-        if text in ["*", "_"]: # just evaluating begin and end and I think this eliminats the == * which looks at one symbol
-            in_italic = not in_italic # we toggle because we don't know if this is the beginning symbol or ending symbol. but we do know what state we've set it too
-            if not in_italic and holding_string:
-                list_of_dictionaries.append({
-                    'type' : 'italic',
-                    'content' : holding_string
-                })
-                holding_string = ""
-            continue # add this to ensure that the markers aren't appended. without this, the 
-        # if we are in block, just keep adding text!
-        if in_italic:
-            holding_string += text
-
-    # keep this here for text that's still italic when we reach the end of the line
-    if in_italic and holding_string:
-        list_of_dictionaries.append({
-            'type' : 'italic',
-            'content' : holding_string
+    # Refactored the read_markdown and created a new process_line function
+    def read_markdown(self, file : str):
+        with open(file, 'r') as md:
+            for line in md:
+                self.process_line(line.strip())
+    
+    def process_line(self, line):
+        for pattern, handler in self.markdown_handlers.items():
+            if line.startswith(pattern):
+                handler(line)
+                break
+    
+    # Rewrite handle_heading function to accomodate all heading types in one function
+    def handle_heading(self, line, level):
+        if level <= 6:
+            stripped_line = line.lstrip('#' * level).strip() # depending on the level passed into the function/what's recognized
+            self.list_of_dictionaries.append({'type' : f'heading{level}', 'content' : stripped_line}) # have to get better at thinking about this
+        else: raise ValueError ("More than acceptable pound signs")
+    
+    def handle_blockquote(self, line):
+        self.list_of_dictionaries.append({
+            'type' : 'blockquote',
+            'content' : line.lstrip('> ').strip()
         })
-    
-    holding_string = "" # confirm that this is needed, I thought the reset happens in above code
 
-# Needed help/review of how to use regex backticking and search for this function implementation
-def handle_inline_block(line):
-    # first step is to define the state
-    in_inline = False 
-    holding_string = "" # figure out why the previous one wasn't persisting
-    collected_inline = ""
-    index = 0
+    def handle_links(self, line):
+        # We need to introduce control flow because there's a limited amount we can do with dictionaries and custom strings
+        # So, anything that matches '[' will be "flagged" as a link. We want to check if this is actually a link or not so we evaluate using if.
+        # if http or https is in the line, evaluate it like a link 
+        # recall that commands for re.search(expression, applied to what)
+        if re.search(r'(http[s]?:\/\/[^\)]+)\)', line): # r here means raw strings
+            pattern = re.findall(r'\[([^\]]+)\]\((http[s]?:\/\/[^\)]+)\)', line) # I originally used .search() but changed to .findall() in the case there are multiple links in a given line.  
+            for link_text, url in pattern: # iterate as a method to unpack tuple created in pattern
+                self.list_of_dictionaries.append({
+                    'type' : 'link', 
+                    'content' : {'link text' : link_text, 'url' : url}
+                })
 
-    # just as before we need to figure out if it's a real or unreal inline code block but isn't this why we have the count between opening and closing?
+    def handle_image(self, line):
+        # I want to search the string to see if I can find any image file types
+        # Learning 5: Turns out we cannot add lists to regex, have to use the or operator instead.
+        if re.search(r'(https?:\/\/[^\s]+?(?:\.png|\.jpg|\.jpeg|\.gif|\.bmp|\.tiff|\.svg))\)', line):
+            match = re.findall(r'!\[([^\]]*)\]\((https?:\/\/[^\s]+?(?:\.png|\.jpg|\.jpeg|\.gif|\.bmp|\.tiff|\.svg))\)', line) # designed to work with the alt text and image syntax, this is why we're looping with two variables
+            for alt_text, image_url in match:
+                self.list_of_dictionaries.append({
+                    'type' : 'image',
+                    'content' : {'alt text' : alt_text, 'image url' : image_url}
+                })        
 
-    while index < len(line):
-        char = line[index]
-
-        if char == '`':
-            backtick_count = 1
-            while index + backtick_count < len(line) and line[index + backtick_count] == '`':
-                backtick_count += 1
-        
-            if not in_inline:
-                in_inline = True
-                opening_backtick_count = backtick_count
-        
-            else:
-                if backtick_count == opening_backtick_count:
-                    in_inline = False
-                    collected_inline += holding_string
-                    list_of_dictionaries.append({
-                        'type' : 'inline code',
-                        'content' : collected_inline
+    # Needed help/review for this function. State managing was tricky. 
+    def handle_italic(self, line):
+        in_italic = False # set the state to False because we haven't encountered asterik yet
+        holding_string = "" # this is the string that gets appended to dictionary
+        for text in line:
+            if text in ["*", "_"]: # just evaluating begin and end and I think this eliminats the == * which looks at one symbol
+                in_italic = not in_italic # we toggle because we don't know if this is the beginning symbol or ending symbol. but we do know what state we've set it too
+                if not in_italic and holding_string:
+                    self.list_of_dictionaries.append({
+                        'type' : 'italic',
+                        'content' : holding_string
                     })
-                
                     holding_string = ""
-                else:
-                    holding_string += '`' * backtick_count
+                continue # add this to ensure that the markers aren't appended. without this, the 
+            # if we are in block, just keep adding text!
+            if in_italic:
+                holding_string += text
+
+        # keep this here for text that's still italic when we reach the end of the line
+        if in_italic and holding_string:
+            self.list_of_dictionaries.append({
+                'type' : 'italic',
+                'content' : holding_string
+            })
         
-            index += backtick_count - 1 # confirm 
+        holding_string = "" # confirm that this is needed, I thought the reset happens in above code
 
-        elif in_inline:
-            holding_string += char
-    
-    index += 1
+    # Needed help/review of how to use regex backticking and search for this function implementation
+    def handle_inline_block(self, line):
+        # first step is to define the state
+        in_inline = False 
+        holding_string = "" # figure out why the previous one wasn't persisting
+        collected_inline = ""
+        index = 0
 
+        # just as before we need to figure out if it's a real or unreal inline code block but isn't this why we have the count between opening and closing?
 
-# Learning 3: Because a dictionary needs to be hashable and immutable, it can only contain static values (strings, numbers, etc)
-# So, re isn't allowed because it's dynamic (pattern matching against multiple strings)
+        while index < len(line):
+            char = line[index]
 
-# How do we handle just text, lol?
-markdown_handlers = {
-    '# ' : handle_heading1, 
-    '## ' : handle_heading2, 
-    '### ' : handle_heading3, 
-    '#### ' : handle_heading4, 
-    '##### ' : handle_heading5,
-    '###### ' : handle_heading6,
-    '``' : handle_inline_block,  
-    '```' : handle_code_block, # this is state-based (hard)
-    '- ' : handle_ullist_block, # this is state-based (hard and review this because there are multiple symbols we can use to define an ul list, also look at ol list syntax)
-    '![' : handle_image, 
-    '> ' : handle_blockquote,
-    '**' : handle_bold, # this is state-based
-    '*' : handle_italic,
-    '[' : handle_links
-}
+            if char == '`':
+                backtick_count = 1
+                while index + backtick_count < len(line) and line[index + backtick_count] == '`':
+                    backtick_count += 1
+            
+                if not in_inline:
+                    in_inline = True
+                    opening_backtick_count = backtick_count
+            
+                else:
+                    if backtick_count == opening_backtick_count:
+                        in_inline = False
+                        collected_inline += holding_string
+                        self.list_of_dictionaries.append({
+                            'type' : 'inline code',
+                            'content' : collected_inline
+                        })
+                    
+                        holding_string = ""
+                    else:
+                        holding_string += '`' * backtick_count
+            
+                index += backtick_count - 1 # confirm 
 
-# Need a central loop or parsing function to work with handle_image v. handle_links
-# Iterator is going to be down here and I need to think about what to handle plain text with.
+            elif in_inline:
+                holding_string += char
+        
+        index += 1
